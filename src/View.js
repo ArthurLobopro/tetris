1745b787103a5797316f2ec5e4a7034788a79d9f
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-05 20:13:54 -0300

    add next figure draw functions

--- src/View.js
+++ src/View.js
@@ -1,37 +1,83 @@
 import { game } from './Game.js'
 import { range } from './Util.js'
 
-const canvas = document.querySelector('canvas')
-const ctx = canvas.getContext('2d')
+const gameCanvas = document.querySelector('canvas#game')
+const gameCtx = gameCanvas.getContext('2d')
+
+const nextCanvas = document.getElementById('next')
+const nextCtx = nextCanvas.getContext('2d')
+
+
+const drawNextFigure = () => {
+    const { squareWidth } = game
+    const { width, height } = game.nextCanvasSize
+    const { figure, color } = game.nextFigure
+
+    const x = Math.trunc(width / 2 - figure[0].length / 2)
+    const y = Math.trunc(height / 2 - figure.length / 2)
+
+    figure.forEach((line, indexY) => {
+        line.forEach((block, indexX) => {
+            if (block.type === 'block') {
+                nextCtx.fillStyle = color
+                nextCtx.fillRect(
+                    (x + indexX) * squareWidth + (1 * x + indexX),
+                    (y + indexY) * squareWidth + (1 * y + indexY),
+                    squareWidth, squareWidth
+                )
+            }
+        })
+    })
+}
+
+const drawNextLines = () => {
+    const { squareWidth } = game
+    const { width, height } = game.nextCanvasSize
+
+    nextCtx.fillStyle = '#aaa'
+
+    for (let i of range(1, width)) {
+        nextCtx.fillRect(i * squareWidth + (i - 1), 0, 1, nextCanvas.height)
+    }
+
+    for (let i of range(1, height)) {
+        nextCtx.fillRect(0, i * squareWidth + (i - 1), nextCanvas.width, 1)
+    }
+}
+
+const drawNextBackground = () => {
+    nextCtx.fillStyle = '#1E1E1E'
+    nextCtx.fillRect(0, 0, nextCanvas.width, nextCanvas.height)
+}
 
 const drawBackground = () => {
-    ctx.fillStyle = '#1E1E1E'
-    ctx.fillRect(0,0, canvas.width, canvas.height)
+    gameCtx.fillStyle = '#1E1E1E'
+    gameCtx.fillRect(0, 0, gameCanvas.width, gameCanvas.height)
 }
 
 const drawLines = () => {
     const { width, height, squareWidth } = game
 
-    ctx.fillStyle = '#bbb'
-    
-    for(let i of range(1, width)){
-        ctx.fillRect(i * squareWidth + (i - 1), 0, 1, canvas.height)
+    gameCtx.fillStyle = '#aaa'
+
+    for (let i of range(1, width)) {
+        gameCtx.fillRect(i * squareWidth + (i - 1), 0, 1, gameCanvas.height)
     }
 
-    for(let i of range(1, height)){
-        ctx.fillRect(0, i * squareWidth + (i - 1), canvas.width, 1)
+    for (let i of range(1, height)) {
+        gameCtx.fillRect(0, i * squareWidth + (i - 1), gameCanvas.width, 1)
     }
 }
 
 const drawSquares = () => {
     const { state, squareWidth } = game
-    state.forEach( (line, indexY) => {
-        line.forEach( (block, indexX) => {
+    state.forEach((line, indexY) => {
+        line.forEach((block, indexX) => {
             const color = block.type === "null" ? "#1e1e1e" : block.color
 
-            ctx.fillStyle = color
-            ctx.fillRect(indexX * squareWidth + (1 * indexX), indexY * squareWidth + (1 * indexY), squareWidth, squareWidth)
-            
+            gameCtx.fillStyle = color
+            gameCtx.fillRect(indexX * squareWidth + (1 * indexX), indexY * squareWidth + (1 * indexY), squareWidth, squareWidth)
+
         })
 
     })
@@ -39,17 +85,15 @@ const drawSquares = () => {
 
 const drawAtualFigure = () => {
     const { squareWidth } = game
-    const { x, y , figure, color } = game.atualFigure
+    const { x, y, figure, color } = game.atualFigure
 
-    
-    
-    figure.forEach( ( line, indexY ) => {
-        line.forEach( (block, indexX) => {
-            if(block.type === 'block'){
-                ctx.fillStyle =  color
-                ctx.fillRect(
-                    ( x + indexX) * squareWidth + (1 * x + indexX),
-                    ( y + indexY ) * squareWidth + (1 * y + indexY),
+    figure.forEach((line, indexY) => {
+        line.forEach((block, indexX) => {
+            if (block.type === 'block') {
+                gameCtx.fillStyle = color
+                gameCtx.fillRect(
+                    (x + indexX) * squareWidth + (1 * x + indexX),
+                    (y + indexY) * squareWidth + (1 * y + indexY),
                     squareWidth, squareWidth
                 )
             }
@@ -58,10 +102,16 @@ const drawAtualFigure = () => {
 }
 
 const renderAll = () => {
+    // Main game
     drawBackground()
     drawLines()
     drawSquares()
     drawAtualFigure()
+
+    //Next figure
+    drawNextBackground()
+    drawNextLines()
+    drawNextFigure()
 }
 
 export { renderAll }
