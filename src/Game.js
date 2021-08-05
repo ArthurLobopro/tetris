703c35ebb82b817ad90a8114909a88c8da15fcb9
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-05 11:57:07 -0300

    add figure spawn

--- src/Game.js
+++ src/Game.js
@@ -7,6 +7,12 @@ const game = {
     width: 15,
     squareWidth: 16,
     state: []
+    atualFigure: {
+        figure: [[]],
+        x: 0,
+        y: 0,
+        color: '#fff'
+    },
 }
 
 const getNewGameState = () => {
@@ -27,7 +33,13 @@ const getNewGameState = () => {
     return table
 }
 
+const spawnNewFigure = () => {
+    game.atualFigure.y = 0
+    game.atualFigure.figure = figures.random()
+    game.atualFigure.x = Math.trunc(game.width / 2 - game.atualFigure.figure[0].length / 2)
+}
 game.state = getNewGameState()
+spawnNewFigure()
 
 canvas.width = (game.width * game.squareWidth) + game.width - 1
 canvas.height = (game.height * game.squareWidth) + game.height - 1
