deabf165fddbb99ac8246fd49b575e0d153fa642
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-05 20:12:04 -0300

    Game.js adapted to next figure canvas

--- src/Game.js
+++ src/Game.js
@@ -2,7 +2,9 @@ import { range } from "./Util.js"
 import { figures } from "./Figures.js"
 import "./Controllers.js"
 
-const canvas = document.getElementById('game')
+const gameCanvas = document.getElementById('game')
+const nextCanvas = document.getElementById('next')
+const pontosSpan = document.getElementById('pontos')
 
 const game = {
     height: 30,
@@ -10,11 +12,20 @@ const game = {
     squareWidth: 16,
     state: [],
     interval: null,
+    pontos: 0,
+    nextFigure: {
+        figure: figures.random(),
+        color: '#ddd'
+    },
+    nextCanvasSize: {
+        height: 6,
+        width: 6
+    },
     atualFigure: {
         figure: [[]],
         x: 0,
         y: 0,
-        color: '#fff'
+        color: '#ddd'
     },
     moveLock: false,
     move(direction) {
@@ -85,7 +96,8 @@ const getNewGameState = () => {
 
 const spawnNewFigure = () => {
     game.atualFigure.y = 0
-    game.atualFigure.figure = figures.random()
+    game.atualFigure.figure = game.nextFigure.figure
+    game.nextFigure.figure = figures.random()
     game.atualFigure.x = Math.trunc(game.width / 2 - game.atualFigure.figure[0].length / 2)
 }
 
@@ -175,8 +187,11 @@ const playGame = () => {
 game.state = getNewGameState()
 spawnNewFigure()
 
-canvas.width = (game.width * game.squareWidth) + game.width - 1
-canvas.height = (game.height * game.squareWidth) + game.height - 1
+gameCanvas.width = (game.width * game.squareWidth) + game.width - 1
+gameCanvas.height = (game.height * game.squareWidth) + game.height - 1
+
+nextCanvas.width = (game.squareWidth * game.nextCanvasSize.width) + game.nextCanvasSize.width - 1
+nextCanvas.height = (game.squareWidth * game.nextCanvasSize.height) + game.nextCanvasSize.height - 1
 
 game.interval = setInterval(playGame, 500);
 
