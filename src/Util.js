3a195ff1759cd1278d12e334c963fbde692d3953
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-08-10 12:36:51 -0300

    add loadAudio

--- src/Util.js
+++ src/Util.js
@@ -1,3 +1,8 @@
+const loadAudio = async path => {
+    const audio = new Audio(path)
+    return audio
+}
+
 const range = (min,max,pass=1) => {
     let array = []
     if(min > max && pass > 0){ pass*=-1 }
@@ -7,4 +12,4 @@ const range = (min,max,pass=1) => {
 
 const randint = (min,max) => Math.floor(Math.random() * (max-min+1)) + min
 
-export { range, randint }
+export { range, randint, loadAudio }
