41fd84eaccca55c34be9544766e44c369c431890
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-12-15 18:59:47 -0300

    refactor: add name to figures without break the game

--- src/render/Figures.js
+++ src/render/Figures.js
@@ -4,94 +4,116 @@ const figures = {
     types: [],
     random() {
         const max = this.types.length - 1
-        return this.types[randint(0, max)]
+        return this.types[randint(0, max)].blocks
     }
 }
 
-figures.types.push([
-    [
-        { type: 'block' },
-        { type: 'block' }
-    ],
-    [
-        { type: 'block' },
-        { type: 'block' }
+figures.types.push({
+    name: "square",
+    blocks: [
+        [
+            { type: 'block' },
+            { type: 'block' }
+        ],
+        [
+            { type: 'block' },
+            { type: 'block' }
+        ]
     ]
-])
+})
 
-figures.types.push([
-    [
-        { type: 'block' },
-        { type: 'block' },
-        { type: 'block' },
-        { type: 'block' }
+figures.types.push({
+    name: "stick",
+    blocks: [
+        [
+            { type: 'block' },
+            { type: 'block' },
+            { type: 'block' },
+            { type: 'block' }
+        ]
     ]
-])
+})
 
-figures.types.push([
-    [
-        { type: 'block' },
-        { type: 'block' },
-        { type: 'null' }
-    ],
-    [
-        { type: 'null' },
-        { type: 'block' },
-        { type: 'block' }
+figures.types.push({
+    name: "z",
+    blocks: [
+        [
+            { type: 'block' },
+            { type: 'block' },
+            { type: 'null' }
+        ],
+        [
+            { type: 'null' },
+            { type: 'block' },
+            { type: 'block' }
+        ]
+    
     ]
+})
 
-])
-
-figures.types.push([
-    [
-        { type: 'null' },
-        { type: 'block' },
-        { type: 'block' }
-    ],
-    [
-        { type: 'block' },
-        { type: 'block' },
-        { type: 'null' }
+figures.types.push({
+    name: "reverse-z",
+    blocks: [
+        [
+            { type: 'null' },
+            { type: 'block' },
+            { type: 'block' }
+        ],
+        [
+            { type: 'block' },
+            { type: 'block' },
+            { type: 'null' }
+        ]
+    
     ]
+})
 
-])
-
-figures.types.push([
-    [
-        { type: 'block' },
-        { type: 'null' },
-        { type: 'null' }
-    ],
-    [
-        { type: 'block' },
-        { type: 'block' },
-        { type: 'block' }
+figures.types.push({
+    name: "reverse-L",
+    blocks: [
+        [
+            { type: 'block' },
+            { type: 'null' },
+            { type: 'null' }
+        ],
+        [
+            { type: 'block' },
+            { type: 'block' },
+            { type: 'block' }
+        ]
     ]
-])
+})
 
-figures.types.push([
-    [
-        { type: 'null' },
-        { type: 'null' },
-        { type: 'block' }
-    ],
-    [
-        { type: 'block' },
-        { type: 'block' },
-        { type: 'block' }
+figures.types.push({
+    name: "L",
+    blocks: [
+        [
+            { type: 'null' },
+            { type: 'null' },
+            { type: 'block' }
+        ],
+        [
+            { type: 'block' },
+            { type: 'block' },
+            { type: 'block' }
+        ]
     ]
-])
+})
 
-figures.types.push([
-    [
-        { type: 'null' },
-        { type: 'block' },
-        { type: 'null' }
-    ],
-    [
-        { type: 'block' },
-        { type: 'block' },
-        { type: 'block' }
+figures.types.push({
+    name: "T",
+    blocks: [
+        [
+            { type: 'null' },
+            { type: 'block' },
+            { type: 'null' }
+        ],
+        [
+            { type: 'block' },
+            { type: 'block' },
+            { type: 'block' }
+        ]
     ]
-])
+})
+
 export { figures }
