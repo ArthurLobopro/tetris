c36bb42e3f8d7068d1298be5996f242d780d61e1
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2021-12-17 12:45:31 -0300

    chore: expose require in render process

--- src/main/preload.js
+++ src/main/preload.js
@@ -1,7 +1,9 @@
 const { ipcRenderer, contextBridge } = require('electron')
 const Store = require("./Store")
 
+contextBridge.exposeInMainWorld("require", require)
+
 document.addEventListener('DOMContentLoaded', () => {
     contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer)
-    contextBridge.exposeInMainWorld('api', Store)
+    contextBridge.exposeInMainWorld('Store', Store)
 })
