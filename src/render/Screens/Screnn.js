bc1f6f3723e1d970410d7b7d5e77f999d0886752
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-01-10 22:41:32 -0300

    refactor: create Screen class

---
+++ src/render/Screens/Screnn.js
@@ -0,0 +1,27 @@
+import navigation from "./navigation.js"
+
+const container = document.getElementById("container")
+
+export class Screen {
+    
+    addNavigation() {
+        window.onkeydown = event => navigation[event.key]?.(this.screen)
+    }
+
+    reset() {
+        this.screen = this.buildFunction()
+    }
+
+    show() {
+        container.appendChild(this.screen)
+    }
+
+    hide() {
+        container.removeChild(this.screen)
+    }
+
+    close() {
+        this.hide()
+        this.reset()
+    }
+}
