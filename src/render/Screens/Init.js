1334227f91692c5a911ee6c95ab5b908df47f48c
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-01-11 12:45:43 -0300

    refactor: init screen adapted to new screen method

--- src/render/Screens/Init.js
+++ src/render/Screens/Init.js
@@ -1,34 +1,45 @@
-import navigationFunctions from "./navigation.js"
-import viewConfig from "./Config.js"
+import { Screen } from "./Screnn.js"
+import { newGame } from "../Game.js"
+import { screens } from "../ScreenManager.js"
 
-const container = document.getElementById('container')
-const get = id => document.getElementById(id)
+export default class InitScreen extends Screen {
+    constructor() {
+        super()
 
-export default async function viewInit() {
-    const init_wrapper = document.createElement('div')
-    init_wrapper.className = "telas-wrapper"
-    init_wrapper.innerHTML = `
-    <fieldset id="init">
-        <legend>Início</legend>
-        <div class="button-wrapper">
-            <button id="start" class="focus">START</button>
-            <button id="config">CONFIGURAÇÕES</button>
-            <button id="exit">SAIR</button>
-        </div>
-    </fieldset>`
-    container.appendChild(init_wrapper)
-    window.onkeydown = event => navigationFunctions[event.key]?.(init_wrapper)
-    return new Promise( res => {
-        get('start').onclick = () => {
-            container.removeChild(init_wrapper)
-            res(true)
+        this.buildFunction = function () {
+            const initScreen = document.createElement('div')
+            initScreen.className = "telas-wrapper"
+            initScreen.innerHTML = `
+            <fieldset id="init">
+                <legend>Início</legend>
+                <div class="button-wrapper">
+                    <button id="start" class="focus">START</button>
+                    <button id="config">CONFIGURAÇÕES</button>
+                    <button id="exit">SAIR</button>
+                </div>
+            </fieldset>`
+
+            const buttons = initScreen.querySelectorAll('button')
+            buttons.forEach(button => {
+                button.onclick = () => {
+                    switch (button.id) {
+                        case "start":
+                            this.close()
+                            newGame()
+                            break
+                        case "config":
+                            screens.config.show()
+                            break
+                        case "exit":
+                            ipcRenderer.send('close')
+                            break
+                    }
+                }
+            })
+
+            return initScreen
         }
-        get('config').onclick = async () => {
-            await viewConfig()
-            window.onkeydown = event => navigationFunctions[event.key]?.(init_wrapper)
-        }
-        get('exit').onclick = () => {
-            ipcRenderer.send('close')
-        }
-    })
+
+        this.reset()
+    }
 }
