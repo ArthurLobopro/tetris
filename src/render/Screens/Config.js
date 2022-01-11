1e58460d8b6b0eb946f1ac1a0bc2dea36d7c4242
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-01-11 10:51:11 -0300

    fix: little error of my part

--- src/render/Screens/Config.js
+++ src/render/Screens/Config.js
@@ -18,34 +18,39 @@ const configs = {
 export default class ConfigScreen extends Screen {
     constructor() {
         super()
-        const configScreen = document.createElement('div')
-        configScreen.className = "telas-wrapper"
-        configScreen.innerHTML = `
-        <fieldset>
-            <legend>CONFIGURAÇÕES</legend>
-            <div class="button-wrapper">
-                <button data-type="music" class="focus">Musica</button>
-                <button data-type="velocity">Velocidade</button>
-                <button data-type="theme">Tema</button>
-                <button data-type="voltar">Voltar</button>
-            </div>
-        </fieldset>`
 
-        this.screen = configScreen
-        this.addNavigation()
-        
-        const buttons = configScreen.querySelectorAll('button')
-        buttons.forEach(button => {
-            button.onclick = async event => {
-                const { type } = event.target.dataset
-                if (type === "voltar") {
-                    reloadGameConfig()
-                    this.close()
-                } else {
-                    await configs[type]?.(game)
-                    this.addNavigation()
+        this.buildFunction = function () {
+            const configScreen = document.createElement('div')
+            configScreen.className = "telas-wrapper"
+            configScreen.innerHTML = `
+            <fieldset>
+                <legend>CONFIGURAÇÕES</legend>
+                <div class="button-wrapper">
+                    <button data-type="music" class="focus">Musica</button>
+                    <button data-type="velocity">Velocidade</button>
+                    <button data-type="theme">Tema</button>
+                    <button data-type="voltar">Voltar</button>
+                </div>
+            </fieldset>`
+
+
+            const buttons = configScreen.querySelectorAll('button')
+            buttons.forEach(button => {
+                button.onclick = async event => {
+                    const { type } = event.target.dataset
+                    if (type === "voltar") {
+                        reloadGameConfig()
+                        this.close()
+                    } else {
+                        await configs[type]?.(game)
+                        this.addNavigation()
+                    }
                 }
-            }
-        })
+            })
+
+            return configScreen
+        }
+
+        this.reset()
     }
 }
