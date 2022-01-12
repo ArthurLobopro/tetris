0cc76701dea7a875f963cedbe5631c673f4561c0
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-01-12 14:31:44 -0300

    fix style bug

--- src/render/Screens/GameOver.js
+++ src/render/Screens/GameOver.js
@@ -12,16 +12,14 @@ export default class GameOverScrenn extends Screen {
             const gameOverScrenn = document.createElement('div')
             gameOverScrenn.className = 'telas-wrapper'
             gameOverScrenn.innerHTML = `
-            <fieldset style="height: 90%">
+            <fieldset id="game-over">
                 <legend>GAME OVER</legend>
-                <div class="table">
-                    <div class="line">
-                        <div>Pontuação:</div>
-                        <div>${game.pontos}</div>
-                    </div>
+                <div class="line">
+                    <div>Pontuação:</div>
+                    <div>${game.pontos}</div>
                 </div>
                 <img src="${image}" style="border: 1px solid #aaa;max-height:73%" id="game-over-print">
-                <button class="focus">NEW GAME</button>
+                <button class="focus" style="align-self: center;margin-bottom: 10px;">NEW GAME</button>
             </fieldset>`
 
 
