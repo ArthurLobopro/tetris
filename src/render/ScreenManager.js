1e4c76b7fea1d151cfae0ceb3fa222ce8f94e869
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-01-19 11:55:05 -0300

    refactor: update game screen to new game method

--- src/render/ScreenManager.js
+++ src/render/ScreenManager.js
@@ -2,22 +2,19 @@ import ConfigScreen from "./Screens/Config.js";
 import InitScreen from "./Screens/Init.js";
 import GameOverScrenn from "./Screens/GameOver.js";
 import PauseScreen from "./Screens/Pause.js";
-import { viewGameScreen } from "./Screens/GameScreen.js";
+import { GameScreen } from "./Screens/GameScreen.js";
 import MusicConfigScreen from "./Screens/Config/Music.js";
 import VelocityConfigScreen from "./Screens/Config/Velocity.js";
 import ThemeConfigScreen from "./Screens/Config/Theme.js";
 import CustomThemeConfigScreen from "./Screens/Config/CustomTheme.js"
-
-const container = document.getElementById("container")
-
-const { gameScreen, gameCanvas, gameCtx, nextCanvas, nextCtx } = viewGameScreen()
-container.appendChild(gameScreen)
 
 const screens = {
     config: new ConfigScreen(),
     init: new InitScreen(),
     gameOver: new GameOverScrenn(),
     pause: new PauseScreen(),
+    game: new GameScreen(),
+    
     configScrenns: {
         music: new MusicConfigScreen(),
         velocity: new VelocityConfigScreen(),
@@ -25,5 +22,8 @@ const screens = {
         customTheme: new CustomThemeConfigScreen()
     }
 }
+
+const { gameScreen, gameCanvas, gameCtx, nextCanvas, nextCtx } = screens.game.getComponents()
+screens.game.show(false)
 
 export { gameScreen, gameCanvas, gameCtx, nextCanvas, nextCtx, screens }
