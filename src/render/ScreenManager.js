1ac65ade4fe456ccb9dbd9b6455e94708c4b69f3
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-02-19 10:30:19 -0300

    refactor: update Game Screen

--- src/render/ScreenManager.js
+++ src/render/ScreenManager.js
@@ -2,7 +2,7 @@ import ConfigScreen from "./Screens/Config.js";
 import InitScreen from "./Screens/Init.js";
 import GameOverScreen from "./Screens/GameOver.js";
 import PauseScreen from "./Screens/Pause.js";
-import { GameScreen } from "./Screens/GameScreen.js";
+import { gameScreen } from "./Screens/GameScreen.js";
 import MusicConfigScreen from "./Screens/Config/Music.js";
 import VelocityConfigScreen from "./Screens/Config/Velocity.js";
 import ThemeConfigScreen from "./Screens/Config/Theme.js";
@@ -15,7 +15,7 @@ const screens = {
     init: new InitScreen(),
     gameOver: new GameOverScreen(),
     pause: new PauseScreen(),
-    game: new GameScreen(),
+    game: gameScreen,
     about: new AboutScreen(),
     controls: new ControlsScreen(),
     configScreens: {
@@ -26,6 +26,4 @@ const screens = {
     }
 }
 
-const { gameScreen, gameCanvas, gameCtx, nextCanvas, nextCtx, last_points_span, points_span } = screens.game.getComponents()
-
-export { gameScreen, gameCanvas, gameCtx, nextCanvas, nextCtx, screens, last_points_span,points_span }
+export { screens }
