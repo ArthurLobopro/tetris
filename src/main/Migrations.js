b29df8e1118e58b814b6071a21c5cd7ac4668865
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2022-03-23 11:07:58 -0300

    fix: no validation in migration

--- src/main/Migrations.js
+++ src/main/Migrations.js
@@ -1,15 +1,17 @@
 const userPreferencesMigrations = {
     "1.1.3": store => {
-        const gameplayVelocity = store.store.gameplayVelocity
+        if (store.has("gameplayVelocity")) {
+            const gameplayVelocity = store.store.gameplayVelocity
 
-        const velocities = {
-            500: "slow",
-            300: "normal",
-            150: "fast"
+            const velocities = {
+                500: "slow",
+                300: "normal",
+                150: "fast"
+            }
+
+            store.delete('gameplayVelocity')
+            store.set('velocity', velocities[gameplayVelocity])
         }
-
-        store.delete('gameplayVelocity')
-        store.set('velocity', velocities[gameplayVelocity])
     }
 }
 
