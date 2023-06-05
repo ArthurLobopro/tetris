e68542c3a3bbe7bdca5791fe2dac716f6050144a
Arthur Lobo <64273139+ArthurLobopro@users.noreply.github.com>
2023-06-05 18:04:00 -0300

    refactor: use electron fonts

--- src/main/preload.js
+++ src/main/preload.js
@@ -5,3 +5,11 @@ contextBridge.exposeInMainWorld("require", require)
 contextBridge.exposeInMainWorld('openExternal', shell.openExternal)
 contextBridge.exposeInMainWorld('Store', Store)
 contextBridge.exposeInMainWorld('ipcRenderer', ipcRenderer)
+
+const NunitoFont = require("@electron-fonts/nunito")
+const PressStart2PFont = require("@electron-fonts/press-start-2p")
+
+window.addEventListener("DOMContentLoaded", () => {
+    NunitoFont.inject()
+    PressStart2PFont.inject()
+})
